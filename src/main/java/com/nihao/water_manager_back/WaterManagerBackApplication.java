b48c59ce374d8bf626b438ad8183856f2c8c04c9
TWJ <1620785675@qq.com>
2024-07-02 08:41:56 +0800

    初始化

---
+++ src/main/java/com/nihao/water_manager_back/WaterManagerBackApplication.java
@@ -0,0 +1,13 @@
+package com.nihao.water_manager_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterManagerBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterManagerBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nihao/water_manager_back/WaterManagerBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nihao.water_manager_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterManagerBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
